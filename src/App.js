edf703f5675e646ce3790331638961293bc0dafc
Chris Marino <christopher.c.marino@gmail.com>
2024-01-27 11:57:25 -0800

    refactored to take minipool address array

--- src/App.js
+++ src/App.js
@@ -1,44 +1,24 @@
 import logo from './logo.svg';
-import axios, { all } from 'axios';
 import './App.css';
 import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
-import irr from "./irr.js";
-let appUrl = process.env.REACT_APP_ETHERSCAN_URL
-let apikey = process.env.REACT_APP_ETHERSCAN_KEY
-let module = "module=account&";
-let action = "action=txsBeaconWithdrawal&";
+import {useIirr, fetchWithdrawls} from "./irrUtils.js";
 let address1 = "address=0x6841ccfeAf1a9C1c5BD19BAdF0500B99C0BD7E97&";
 let address2 = "address=0xb3684a0BB31Cde887bf02DBFc5738ebAF29a153A&";
 let address3 = "address=0xA87BD09599B1d7Bcc321e0f08C4AE2B48A7Ece4f&";
-let startblock = "startblock=0&";
-let endblock = "endblock=99999999&";
-let page = "page=1&";
-let offset = "offset=100&";
-let sort = "sort=asc&";
-let url1 = (appUrl + module + action + address1 + startblock + endblock + page + offset + sort + "apikey=" + apikey)
-let url2 = (appUrl + module + action + address2 + startblock + endblock + page + offset + sort + "apikey=" + apikey)
-let url3 = (appUrl + module + action + address3 + startblock + endblock + page + offset + sort + "apikey=" + apikey)
-let minipoolArray = [url1, url2, url3];
+let minipoolArray = [address1, address2, address3];
 let allWithdrawls = [];
 let datetest = [];
 function App() {
   const [withdrawls, setWithdrawls] = React.useState([]);
-  async function fetchWithdrawls(url) {
-    try {
-      const payouts = await axios(url);
-      return payouts.data;
-    } catch (error) {
-      console.log("Axios Error:", error);
-    }
-  };
-//console.log("Test payouts.data: ", payouts.data);
+
+  //console.log("Test payouts.data: ", payouts.data);
   const hasRun = useRef(false);
 
   useEffect(() => {
     if (!hasRun.current) {
-      minipoolArray.forEach((url) => {
-        fetchWithdrawls(url).then(oneWithdrawl => {
+      minipoolArray.forEach((address) => {
+        fetchWithdrawls(address).then(oneWithdrawl => {
           allWithdrawls = allWithdrawls.concat(oneWithdrawl.result);
           //console.log("allWithdrawls:", allWithdrawls);
           setWithdrawls(allWithdrawls);
---
+++ src/irrUtils.js
@@ -0,0 +1,56 @@
+// Desc: This hook will calculate the IRR of a given set of cash flows.
+import { convertRate } from 'node-irr';
+import { xirr } from 'node-irr';
+import axios, { all } from 'axios';
+
+export function irr(paymentArray) {
+  // A utility function used to calculate the irr of a given set of cash flows, paymentArray. 
+  // paymentArray is a any any array that incldudes an 'amount' and 'timestamp' key. This arry is 
+  // typically the result of a query to the etherscan API.
+  const startDate = new Date(2021, 5, 1);
+  let year = startDate.getFullYear();
+  let month = String(startDate.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed in JavaScript
+  let day = String(startDate.getDate()).padStart(2, '0');
+  const formattedStartDate = `${year}${month}${day}`;
+
+  const today = new Date();
+  year = today.getFullYear();
+  month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed in JavaScript
+  day = String(today.getDate()).padStart(2, '0');
+  const formattedToday = `${year}${month}${day}`;
+  const data3 = [
+    { amount: -10, date: formattedStartDate },
+    { amount: 11, date: formattedToday },
+  ]
+  const dailyRate = xirr(paymentArray).rate;
+  const days = xirr(paymentArray).days;
+
+  //Balance is uniformly earned over the period.
+
+  //express the rate in APR
+  //let rate = convertRate(dailyRate, "year")
+  let rate = 100 * (data3 / 32E18) * (365 / days); //APR
+  return { days, rate };
+}
+
+export async function fetchWithdrawls(address) {
+  // A utility function used to fetch the withdrawls from the ethscan API. Take a url as an argument.
+  // Note: Probably should this to take minipool address as an argument.
+  let appUrl = process.env.REACT_APP_ETHERSCAN_URL
+  let apikey = process.env.REACT_APP_ETHERSCAN_KEY
+  let module = "module=account&";
+  let action = "action=txsBeaconWithdrawal&";
+  let startblock = "startblock=0&";
+  let endblock = "endblock=99999999&";
+  let page = "page=1&";
+  let offset = "offset=100&";
+  let sort = "sort=asc&";
+  let url = (appUrl + module + action + address + startblock + endblock + page + offset + sort + "apikey=" + apikey)
+  try {
+    let payouts = [];
+    payouts = await axios(url);
+    return payouts.data;
+  } catch (error) {
+    console.log("Axios Error:", error);
+  }
+};
