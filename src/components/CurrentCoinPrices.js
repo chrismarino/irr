4447e1f817720790ff0c9ff0c30fd58a0dac3d27
Chris Marino <christopher.c.marino@gmail.com>
2024-02-11 11:49:01 -0800

    Format coin currency

--- src/components/CurrentCoinPrices.js
+++ src/components/CurrentCoinPrices.js
@@ -4,8 +4,8 @@ import React from 'react';
 function CurrentCoinPrices({ ethPriceNow, rplPriceNow }) {
   return (
     <>
-      <p>Current Ethereum Price: ${(ethPriceNow[0].price_usd.toFixed(2) || [])}</p>
-      <p>Current Rocketpool Price: ${(rplPriceNow[0].price_usd.toFixed(2) || [])}</p>
+      <p>Current Ethereum Price: {(ethPriceNow[0].price_usd.toLocaleString('en-US', { style: 'currency', currency: 'USD' }) || [])}</p>
+      <p>Current Rocketpool Price: {(rplPriceNow[0].price_usd.toLocaleString('en-US', { style: 'currency', currency: 'USD' }) || [])}</p>
     </>
   );
 }
