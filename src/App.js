14d6629d775b70e922c98598c3ae79df1424ba43
Chris Marino <christopher.c.marino@gmail.com>
2024-02-11 07:47:50 -0800

    Restructured to use multiple Components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import './App.css';
 import NodeAPRs from "./components/NodeAPRs";
 import NodeAddressForm from "./components/NodeAddressForm";
-
+import CurrentCoinPrices from './components/CurrentCoinPrices';
+import usePriceNow from './hooks/usePriceNow';
 import { useState, useEffect, useRef } from 'react';
 
 
 function App() {
-  //const [nodeAddress, setNodeAddress] = useState("0x8f7fae807c3c3600fc952b7eadaa3a9a68d5b062");
-  const [gotNewNodeAddress, setGotNewNodeAddress] = useState(false);
   const [nodeAddress, setNodeAddress] = useState("0x635D06a61a36566003D71428F1895e146CdBD54E");
+  const { priceNow: ethPriceNow, gotPriceNow: gotEthPriceNow } = usePriceNow("ethereum");
+  const { priceNow: rplPriceNow, gotPriceNow: gotRplPriceNow } = usePriceNow("rocket-pool");
+
+  if (!gotEthPriceNow || !gotRplPriceNow ) {
+    return <div>Loading currnet Eth and RPL prices...</div>;
+  } 
   //const [nodeAddress, setNodeAddress] = useState("0x8f7fae807c3c3600fc952b7eadaa3a9a68d5b062"); // 2 validators 
   //const [nodeAddress, setNodeAddress] = useState("0xee43198c3be288fddabafefabbd49f6111b175c5"); // 6 validators. Works
 
@@ -19,12 +24,14 @@ function App() {
   //let nodeAddress4 = "0x1829f19524429a2edaf07bd13d1e47af19643d9b"
   //let nodeAddress3 = "0x20a3aba3c6851dd3b4f3c8cd73911cfb0a5e38a4";
   //let nodeAddress5 = "0xd9c2d5c041ad53b8b0d70968da88ecbf5e973cd3"; // more than 20 validators
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Minipool APRs</h1>
+        <CurrentCoinPrices ethPriceNow={ethPriceNow} rplPriceNow={rplPriceNow} />
         <NodeAddressForm setNodeAddress={setNodeAddress} nodeAddress={nodeAddress} />
-        <NodeAPRs nodeAddress={nodeAddress} />
+        <NodeAPRs nodeAddress={nodeAddress} ethPriceNow={ethPriceNow} rplPriceNow={rplPriceNow} /> 
       </header>
     </div>
   );
--- src/components/NodeAPRs.js
+++ src/components/NodeAPRs.js
@@ -1,27 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import APRGrid from "./APRGrid";
 import useMinipoolAPRs from '../hooks/useMinipoolAPRs';
-import usePriceNow from '../hooks/usePriceNow';
-import CurrentCoinPrices from './CurrentCoinPrices';
 
-function NodeAPRs({ nodeAddress }) {
-
-  //const { priceNow: ethPriceNow, gotPriceNow: gotEthPriceNow } = usePriceNow("ethereum");
-  //const { priceNow: rplPriceNow, gotPriceNow: gotRplPriceNow } = usePriceNow("rocket-pool");
-  const { priceNow: ethPriceNow, gotPriceNow: gotEthPriceNow } = usePriceNow("ethereum");
-  const { priceNow: rplPriceNow, gotPriceNow: gotRplPriceNow } = usePriceNow("rocket-pool");
+function NodeAPRs({ nodeAddress, ethPriceNow }) {
   const { nodeAPRs } = useMinipoolAPRs(nodeAddress, ethPriceNow);
-
-  if (!gotEthPriceNow || !gotRplPriceNow ) {
-    return <div>Loading currnet Eth and RPL prices...</div>;
-  } 
+  //console.log("nodeAddress, ethPriceNow in NodeAPRs:", nodeAddress, ethPriceNow)
   if (!nodeAPRs.nodeAPR || !nodeAPRs.nodeOperatorAPR || !nodeAPRs.protocolAPR) {
     return <div>Calculating APRs...</div>;
-  } 
-  
+  }
+
   return (
     <div className="NodeAPRs">
-      <CurrentCoinPrices ethPriceNow={ethPriceNow} rplPriceNow={rplPriceNow} />
+
       <section>
         <p></p><h3>Total Node Returns</h3>
         {<APRGrid rows={(nodeAPRs.nodeAPR || [])} />}
