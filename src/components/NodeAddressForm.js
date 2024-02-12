5d474672cfb2880d51c602198dc57726188983a8
Chris Marino <christopher.c.marino@gmail.com>
2024-02-11 20:42:15 -0800

    Style updates

--- src/components/NodeAddressForm.js
+++ src/components/NodeAddressForm.js
@@ -3,22 +3,25 @@ import React from 'react';
 
 function NodeAddressForm({ setNodeAddress, nodeAddress }) {
   return (
-    <form
-      onSubmit={event => {
-        event.preventDefault();
-        setNodeAddress(event.target.elements.nodeAddress.value);
-      }}
-    >
-      <input
-        type="text"
-        name="nodeAddress"
-        defaultValue={nodeAddress}
-        onClick={event => event.target.select()}
-        placeholder="Enter node address"
-        style={{ marginRight: '10px' }} 
-      />
-      <button type="submit">Enter</button>
-    </form>
+    <div>
+      <p></p><h3>Enter the address of a validator node<br></br>to find its total return</h3>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          setNodeAddress(event.target.elements.nodeAddress.value);
+        }}
+      >
+        <input
+          type="text"
+          name="nodeAddress"
+          defaultValue={nodeAddress}
+          onClick={event => event.target.select()}
+          placeholder="Enter node address"
+          style={{ marginRight: '10px' , width: '330px' }}
+        />
+        <button type="submit">Enter</button>
+      </form>
+    </div>
   );
 }
 
